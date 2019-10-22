adfb5d94e7ef7b78a6ff6af7fe83ea7f28351f0f
Yo Yi <53082431+colinyoyo26@users.noreply.github.com>
2019-10-22 23:58:24 +0800

    Reduce the scope of i & declare loop as const

--- BBP/bench.c
+++ BBP/bench.c
@@ -5,38 +5,39 @@
 #define CLOCK_ID CLOCK_MONOTONIC_RAW
 #define ONE_SEC 1000000000.0
 
-extern double compute_pi_BBP (int pos);
-extern double compute_pi_BBP_openmp (int pos, int threads);
+extern double compute_pi_BBP(int pos);
+extern double compute_pi_BBP_openmp(int pos, int threads);
 
-int main (int argc, char** argv){
+int main(int argc, char **argv)
+{
     int N = atoi(argv[1]);
     struct timespec start = {0, 0};
     struct timespec end = {0, 0};
-    size_t i, loop = 5;
+    const size_t loop = 20;
 
     clock_gettime(CLOCK_ID, &start);
-    for (i = 0; i < loop; i++) {
-        compute_pi_BBP (N);
-    }
-    clock_gettime(CLOCK_ID, &end);
-    printf("%lf ", (double) (end.tv_sec - start.tv_sec) +
-                       (end.tv_nsec - start.tv_nsec) / ONE_SEC);
-    
-    clock_gettime(CLOCK_ID, &start);
-    for (i = 0; i < loop; i++) {
-        compute_pi_BBP_openmp (N, 2);
+    for (size_t i = 0; i < loop; i++) {
+        compute_pi_BBP(N);
     }
     clock_gettime(CLOCK_ID, &end);
     printf("%lf ", (double) (end.tv_sec - start.tv_sec) +
                        (end.tv_nsec - start.tv_nsec) / ONE_SEC);
 
     clock_gettime(CLOCK_ID, &start);
-    for (i = 0; i < loop; i++) {
-        compute_pi_BBP_openmp (N, 4);
+    for (size_t i = 0; i < loop; i++) {
+        compute_pi_BBP_openmp(N, 2);
+    }
+    clock_gettime(CLOCK_ID, &end);
+    printf("%lf ", (double) (end.tv_sec - start.tv_sec) +
+                       (end.tv_nsec - start.tv_nsec) / ONE_SEC);
+
+    clock_gettime(CLOCK_ID, &start);
+    for (size_t i = 0; i < loop; i++) {
+        compute_pi_BBP_openmp(N, 4);
     }
     clock_gettime(CLOCK_ID, &end);
     printf("%lf\n", (double) (end.tv_sec - start.tv_sec) +
-                       (end.tv_nsec - start.tv_nsec) / ONE_SEC);
-    
+                        (end.tv_nsec - start.tv_nsec) / ONE_SEC);
+
     return 0;
 }
